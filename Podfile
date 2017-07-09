fe26e3c6c8bc9c054698436ad86088faac784284
875704719@qq.com <875704719@qq.com>
2017-07-09 12:51:32 +0800

    xigai

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios,  '7.0'
+target "YSHCustomView" do
+
+   
+    pod 'Masonry', '1.0.0'
+    
+  
+end
---
+++ YSHCustomView/YSHCustomView.h
@@ -0,0 +1,19 @@
+//
+//  YSHCustomView.h
+//  YSHCustomView
+//
+//  Created by yanli on 2017/7/9.
+//  Copyright © 2017年 yanli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YSHCustomView.
+FOUNDATION_EXPORT double YSHCustomViewVersionNumber;
+
+//! Project version string for YSHCustomView.
+FOUNDATION_EXPORT const unsigned char YSHCustomViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YSHCustomView/PublicHeader.h>
+
+
